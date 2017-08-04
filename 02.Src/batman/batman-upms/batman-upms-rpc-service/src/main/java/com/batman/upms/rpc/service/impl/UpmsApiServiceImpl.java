9a94409ac3c6308d32372cee887e23dbc43e0fb8
jiangyh <jiangyh@dxcct.com>
2017-08-04 13:29:17 +0800

    add

---
+++ 02.Src/batman/batman-upms/batman-upms-rpc-service/src/main/java/com/batman/upms/rpc/service/impl/UpmsApiServiceImpl.java
@@ -0,0 +1,12 @@
+package com.batman.upms.rpc.service.impl;
+
+import com.batman.upms.dao.model.UpmsUser;
+import com.batman.upms.rpc.api.UpmsApiService;
+
+public class UpmsApiServiceImpl implements UpmsApiService {
+
+    @Override
+    public UpmsUser selectUpmsUserByUsername(String username) {
+        return null;
+    }
+}
