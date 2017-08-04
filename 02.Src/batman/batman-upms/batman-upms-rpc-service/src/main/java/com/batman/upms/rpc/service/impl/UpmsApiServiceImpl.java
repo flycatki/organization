30250fee65c900a8474897cf2fa4a2f6def880d6
jiangyh <jiangyh@dxcct.com>
2017-08-04 15:55:02 +0800

    add sso getuser function

--- 02.Src/batman/batman-upms/batman-upms-rpc-service/src/main/java/com/batman/upms/rpc/service/impl/UpmsApiServiceImpl.java
+++ 02.Src/batman/batman-upms/batman-upms-rpc-service/src/main/java/com/batman/upms/rpc/service/impl/UpmsApiServiceImpl.java
@@ -1,12 +1,27 @@
 package com.batman.upms.rpc.service.impl;
 
+import com.batman.upms.dao.mapper.UpmsUserMapper;
 import com.batman.upms.dao.model.UpmsUser;
+import com.batman.upms.dao.model.UpmsUserExample;
 import com.batman.upms.rpc.api.UpmsApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class UpmsApiServiceImpl implements UpmsApiService {
 
+    @Autowired
+    UpmsUserMapper upmsUserMapper;
+
     @Override
     public UpmsUser selectUpmsUserByUsername(String username) {
+        UpmsUserExample upmsUserExample = new UpmsUserExample();
+        upmsUserExample.createCriteria()
+                .andLoginNameEqualTo(username);
+        List<UpmsUser> upmsUsers = upmsUserMapper.selectByExample(upmsUserExample);
+        if (null != upmsUsers && upmsUsers.size() > 0) {
+            return upmsUsers.get(0);
+        }
         return null;
     }
 }
