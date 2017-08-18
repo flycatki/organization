48f316f9558bbfe737d98db5862221a04f26d5cf
tmy <tanmengyu0811@163.com>
2017-08-18 09:23:10 +0800

    组织机构初始化ajax异步数据疏通初步完成，从后台取出假数据并表现为tree

--- 02.Src/batman/batman-upms/batman-upms-server/src/main/java/com/batman/upms/server/controller/manage/UpmsOrganizationController.java
+++ 02.Src/batman/batman-upms/batman-upms-server/src/main/java/com/batman/upms/server/controller/manage/UpmsOrganizationController.java
@@ -15,51 +15,71 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Controller
 @RequestMapping("/manage/organization")
 public class UpmsOrganizationController extends BaseController {
 
-    @Autowired
-    private UpmsOrganizationService upmsOrganizationService;
+	@Autowired
+	private UpmsOrganizationService upmsOrganizationService;
 
-    @ApiOperation(value = "组织首页")
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String index() {
+	@ApiOperation(value = "组织首页")
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index() {
 
-/*        UpmsOrganization upmsOrganization = new UpmsOrganization();
-        upmsOrganization.setUuid(UUID.randomUUID().toString());
+		/*
+		 * UpmsOrganization upmsOrganization = new UpmsOrganization();
+		 * upmsOrganization.setUuid(UUID.randomUUID().toString());
+		 * 
+		 * 
+		 * int count =
+		 * upmsOrganizationService.insertSelective(upmsOrganization);
+		 * 
+		 * UpmsOrganizationExample example = new UpmsOrganizationExample();
+		 * 
+		 * UpmsOrganizationExample.Criteria criteria = example.createCriteria();
+		 * 
+		 * criteria.andDeleteflagEqualTo(0) .andIdEqualTo("0001");
+		 * 
+		 * upmsOrganizationService.selectByExample(example);
+		 */
+
+		return "manage/organization/index.page";
+	}
+
+	@RequestMapping(value = "/init", method = RequestMethod.GET)
+	@ResponseBody
+	public Object init() {
+//		UpmsOrganizationExample example = new UpmsOrganizationExample();
+//
+//		UpmsOrganizationExample.Criteria criteria = example.createCriteria();
+//
+//		criteria.andDeleteflagEqualTo(0);
+
+		// List<UpmsOrganization> organizations =
+		// upmsOrganizationService.selectByExample(example);
+
+		List<UpmsOrganization> organizations = new ArrayList<UpmsOrganization>();
+
+		for (int i = 1; i < 6; i++) {
+			UpmsOrganization upmsOrganization = new UpmsOrganization();
+			
+			String s1 = String.valueOf(i);
+			String s2 = String.valueOf(i - 1);
+			upmsOrganization.setUuid(s1);
+			upmsOrganization.setName("test" + i);
+			upmsOrganization.setParentUuid(s2);
+			organizations.add(upmsOrganization);
+		}
+		
 
 
-        int count = upmsOrganizationService.insertSelective(upmsOrganization);
-
-        UpmsOrganizationExample example = new UpmsOrganizationExample();
-
-        UpmsOrganizationExample.Criteria criteria = example.createCriteria();
-
-        criteria.andDeleteflagEqualTo(0)
-                .andIdEqualTo("0001");
-
-        upmsOrganizationService.selectByExample(example);*/
-
-
-        return "manage/organization/index.page";
-    }
-    
-    @RequestMapping(value = "/init", method = RequestMethod.POST)
-    @ResponseBody
-    public Object init(){
-    	UpmsOrganizationExample example = new UpmsOrganizationExample();
-
-        UpmsOrganizationExample.Criteria criteria = example.createCriteria();
-        
-        criteria.andDeleteflagEqualTo(0);
-        
-        List<UpmsOrganization> organizations = upmsOrganizationService.selectByExample(example);
-    	
-    	return organizations;
-    }
+		return organizations;
+	}
 
 }
